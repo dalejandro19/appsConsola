b152f851382bded4d57366d538fbd5b3667ff123
dalejandro_win <dalejandro.hernandez19@hotmail.com>
2021-08-23 14:48:08 -0600

    Reestructura del metodo de conversion e implementacion de entradas de dato

--- src/appConsola/Main.java
+++ src/appConsola/Main.java
@@ -39,10 +39,10 @@ public class Main {
 		} while (validacion(strOpcionMenu) == false);
 
 		// MENU
-		switch (Integer.parseInt(strOpcionMenu)) {
+		switch (conversionDato(strOpcionMenu)) {
 		case 1:
-
-			System.out.println("HOLA MUNDO");
+			System.out.println("PRIMERA PARTE");
+			generadorCubetas();
 			break;
 
 		case 3:
@@ -56,7 +56,37 @@ public class Main {
 		scan.close();
 	}
 
-	public Object conversionDato(String cadena) {
+	public void generadorCubetas() {
+		String strCantidadPelotas, strValorLimite, strNumeroPelota;
+		int cantidadPelotas, valorLimite, numeroPelota;
+		Scanner scan = new Scanner(System.in);
+
+		// LIMPIAR CONSOLA
+		do {
+			System.out.println("Ingrese la cantidad de pelotas a ingresar en la cubeta");
+			strCantidadPelotas = scan.next();
+		} while (validacion(strCantidadPelotas) == false);
+
+		// LIMPIAR CONSOLA
+		do {
+			System.out.println("Ingrese el valor limite para las pelotas\nNOTA: Debe ser mayor a cero");
+			strValorLimite = scan.next();
+		} while (validacion(strValorLimite) == false);
+
+		cantidadPelotas = conversionDato(strCantidadPelotas);
+		valorLimite = conversionDato(strValorLimite);
+
+		if (valorLimite < 0 || (cantidadPelotas > 100 || cantidadPelotas <= 0)) {
+			System.out.println("ERROR: INGRESE LOS VALORES NUEVAMENTE");
+			tiempoEspera();
+			generadorCubetas();
+		} else {
+			System.out.println("CANTIDAD DE PELOTAS: " + cantidadPelotas + " - VALOR LIMITE:" + valorLimite);
+		}
+
+	}
+
+	public int conversionDato(String cadena) {
 		int dato;
 		dato = Integer.parseInt(cadena);
 		return dato;
